8e8f25afea2610a00160c60d60eb1d0a80628dba
Roberto Gonzalez <robertogonzalez.ideatigers121@gmail.com>
2025-10-10 18:29:20 -0500

    commit 2

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,26 @@
 <script setup>
-import { ref } from 'vue'
-const title = ref('Hello There ✋')
+import { onMounted } from 'vue'
+import BaseTitle from '@/components/BaseTitle.vue'
+import useAPI from '@/composables/useAPI'
+import { RouterLink } from 'vue-router'
+
+const { categories, getCategories } = useAPI()
+
+onMounted(async () => {
+  await getCategories()
+})
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+  <div class="grid flex-grow grid-cols-4 gap-12 m-20">
+    <RouterLink 
+      v-for="category in categories"
+      :key="category.id"
+      :to="''"
+      class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-4 border-slate-500 py-4 font-bold uppercase hover:cursor-pointer hover:border-red-500 hover:bg-red-400 hover:text-white transition-colors duration-300"
+    >
+      {{ category.name }}
+    </RouterLink>
+  </div>
 </template>
