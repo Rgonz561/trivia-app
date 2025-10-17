09972430111835ecb6c8ac271d0b60e5b9348739
Roberto Gonzalez <robertogonzalez.ideatigers121@gmail.com>
2025-10-17 18:07:48 -0500

    commit 3

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,5 +1,58 @@
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">I'm Other Page</h1>
-  </main>
+  <div v-if="question" class="flex h-full w-full flex-col items-center gap-8 p-10">
+    <BaseTitle>{{ question.category }}</BaseTitle>
+    <!-- {{ question.question }} -->
+    <div v-html="question.question" class="text-center text-2xl font-bold"></div>
+    <div 
+      v-for="answer in answers"
+      v-html="answer.answer"
+      :key="answer.id"
+      class="bg-green-500 flex items-center justify-center text-4xl rounded-lg text-white py-10">
+    </div>
+  </div>
+  <div v-else class="">  
+    Loading...
+  </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+import useAPI from '@/composables/useAPI';
+import { useRoute } from 'vue-router';
+import BaseTitle from '@/components/BaseTitle.vue';
+
+const api = useAPI()
+const question = ref(null)
+const route = useRoute()
+const answers = ref([])
+
+onMounted(async () => {
+  question.value = await api.getQuestion(route.params.id)
+  
+  answers.value.push({
+    id: answers.value.length,
+    correct: true,
+    answer: question.value.correct_answer
+  })
+  
+  question.value.incorrect_answers.map((wrong_answer) => {
+    answers.value.push({
+      id: answers.value.length,
+      correct: false,
+      answer: wrong_answer
+    })
+  })
+  
+  answers.value = shuffle(answers.value)
+  console.log(question.value)
+})
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // swap
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array
+}
+</script>
